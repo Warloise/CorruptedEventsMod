e509f1fb93d18a898a61691432617f21d2d585d4
Warloise <turtlecraft51101@gmail.com>
2025-01-12 18:53:40 +0100

    Glich efect finished

--- src/main/java/klk/warloise/corruptedevents/effects/GlichEffect.java
+++ src/main/java/klk/warloise/corruptedevents/effects/GlichEffect.java
@@ -8,17 +8,15 @@ public class GlichEffect extends MobEffect {
     public GlichEffect(MobEffectCategory category, int color) {
         super(category, color);
     }
-    
+
     @Override
     public void applyEffectTick(LivingEntity entity, int amplifier) {
-        // Aquí defines lo que hace el efecto cada "tick".
-        //Quiero que me haga la pantalla el efecto de glich
-
+        // Por ahora, no necesitamos lógica aquí. Todo se manejará en el cliente.
     }
 
     @Override
     public boolean isDurationEffectTick(int duration, int amplifier) {
-        // Define cuándo se activa el efecto. Este ejemplo activa el efecto cada 20 ticks.
+        // Activar cada 20 ticks (ajustable si se necesita).
         return duration % 20 == 0;
     }
 }
