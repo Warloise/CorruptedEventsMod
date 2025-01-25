7dd8630c7bdd70d236a2b0a3575c0eac6b50c607
Warloise <turtlecraft51101@gmail.com>
2025-01-25 16:04:31 +0100

    Armor Lower ya va

--- src/main/java/klk/warloise/corruptedevents/events/HalfArmorHandler.java
+++ src/main/java/klk/warloise/corruptedevents/events/HalfArmorHandler.java
@@ -3,22 +3,22 @@ package klk.warloise.corruptedevents.events;
 import klk.warloise.corruptedevents.registry.ModEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.FORGE)
-public class DamageLowerHandler {
+public class HalfArmorHandler {
 
     @SubscribeEvent
-    public static void onLivingHurt(LivingHurtEvent event) {
-        if (event.getSource().getEntity() instanceof Player player) {
-            // Verificar si el jugador tiene el efecto activo.
-            if (player.hasEffect(ModEffects.DAMAGE_LOWER.get())) {
-                // Reducir el daño infligido en un 50%.
-                event.setAmount(event.getAmount() * 0.5f);
+    public static void onLivingDamage(LivingHurtEvent event) {
+        if (event.getEntity() instanceof Player player) {
+            if (player.hasEffect(ModEffects.HALF_ARMOR_EFFECT.get())) {
+                event.setAmount(event.getAmount()*2);
+                System.out.println("EntitySoure= "+event.getEntity());
             }
         }
-
     }
 }
